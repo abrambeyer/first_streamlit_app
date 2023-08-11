f95c8e047b98d85226c311ecabe124d486296227
Abram Beyer <abrambeyer@gmail.com>
2023-08-11 12:41:29 -0400

    Update streamlit_app.py
    import pandas
    read_csv to get fruit text file data
    display as pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,19 @@
 import streamlit
+import pandas as pd
 
+
+#page header
 streamlit.title("My Mom's New Healthy Diner")
 
-
+#breakfast menu.  header and menu items.
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
+#second header banner for smoothies promotion
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#read fruit dataset and display
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
