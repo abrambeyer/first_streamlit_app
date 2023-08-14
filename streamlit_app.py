0578de8749bfa6b97a163f83f77e77d008c82ade
Abram Beyer <abrambeyer@gmail.com>
2023-08-13 22:42:01 -0400

    Update streamlit_app.py
    Add Snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import requests
 import snowflake.connector
 
+
+
 #page header
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -49,3 +51,9 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 #output normalized json to screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
