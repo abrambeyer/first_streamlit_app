dc11c8001b56d16afd35e6332c5fbad2d1fd5e92
Abram Beyer <abrambeyer@gmail.com>
2023-08-13 23:08:36 -0400

    Update streamlit_app.py
    Adding try/except and if/else control

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,19 +39,22 @@ streamlit.dataframe(fruits_to_show)
 #header for Fruityvice section.
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    #streamlit.write('The user entered', fruit_choice)
+    #api request for fruityvice data
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #streamlit.text(fruityvice_response.json())  #just writes the json to the screen
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    #output normalized json to screen as a table
+    streamlit.dataframe(fruityvice_normalized)
 
-#api request for fruityvice data
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-#streamlit.text(fruityvice_response.json())  #just writes the json to the screen
-
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-
-#output normalized json to screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
+except URLError as e:
+  streamlit.error()
+  
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
